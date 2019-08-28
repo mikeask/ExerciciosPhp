2fd1d051d1bd76da3c3049b838a725cc2bddb03b
Mikeask <maycon-adm@hotmail.com>
2019-08-28 19:55:14 -0300

    Todos os casos de uso já presentes no exercicio (PHPUnit) cobertos
    Resolvido problemas com string vazia, texto começando com espaço (" ").
    
    Código comentado
    
    Testes pelo Circleci validaram os casos de uso.

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -12,71 +12,58 @@ class Resolucao implements TextWrapInterface {
    */
   public function textWrap(string $text, int $length): array {
 
-    if($text == null || $text=="" || $text == " "){
+    if($text == null || $text=="" || $text == " "){   //se a string de entrada for vazia, retorna null
       return [null];
     }
 
-    $oldLength = $length;
     $newText = "";
-    $textLength = strlen($text);
 
-    // echo "Texto: \t".$text."\n Tamanho: ".$textLength;
-    // echo "<br>";
-
-    $textArray = explode (" ", $text);
+    $textArray = explode (" ", $text);             //separa a string $text por espaço " ", salvando as palavras em um array de strings      
     $line = "";
 
-    foreach ($textArray as &$word) {                    //percorre a lista de palavras
-      // echo "word: ".$word;
-      if(strlen($word) > $oldLength){                   //se a palavra couber em uma linha
-        if(strlen($line) > 0){
-          $line = $line."\n";                         //quebra linha
-          $newText = $newText.$line;                  //coloca a linha com a quebra no texto
-          $line = "";                                 //limpa a linha para começar uma nova
+    foreach ($textArray as &$word) {               //percorre a lista de palavras
+      if(strlen($word) > $length){                  //se a palavra couber em uma linha
+        if(strlen($line) > 0){                          //se a linha tiver algum conteudo
+          $line = $line."\n";                            //quebra linha
+          $newText = $newText.$line;                     //coloca a linha com a quebra no texto
+          $line = "";                                    //limpa a linha para começar uma nova
         }
         $chars = str_split($word);                     //transforma a string $word em um array de chars $chars
-        foreach($chars as $char){                       //percorre o array de chars $chars
-          if(strlen($line) < $oldLength){              //se a linha atual tiver seu tamanho menor que o máximo
-            $line = $line.$char;                        //adiciona o char na linha
+        foreach($chars as &$char){                      //percorre o array de chars $chars
+          if(strlen($line) < $length){               //se a linha atual tiver seu tamanho menor que o máximo
+            $line = $line.$char;                          //adiciona o char na linha
           }else{                                        //se a linha tiver um tamanho maior que o máximo
-            $line = $line."\n";                         //quebra linha
-            // echo "<p> worldLine: ".$line."\n </p>";
-            $newText = $newText.$line;                 //coloca a linha com a quebra no texto
-            $line = "";                                 //limpa a linha para começar uma nova
-            $line = $line.$char;                        //adiciona o caractere que não coube na outra linha na nova
+            $line = $line."\n";                           //quebra linha
+            $newText = $newText.$line;                    //coloca a linha com a quebra no texto
+            $line = "";                                   //limpa a linha para começar uma nova
+            $line = $line.$char;                          //adiciona o caractere que não coube na outra linha na nova
           }
         }
-      }else{
-        // echo "<p> line+word: ".strlen($line." ".$word)."| oldlength: ".$oldLength." </p>";
-        if(strlen($line." ".$word) <= $oldLength){
-          // echo "<p> préWord: ".$line;
-          if(strlen($line) > 0){
-            $line = $line." ".$word;
-          }else{
-            $line = $word;
+      }else{                                            //se a palavra NÃO for maior que o limite por linha
+        if(strlen($line." ".$word) <= $length){        //verifica há espaço para a palavra na linha
+          if(strlen($line) > 0){                            //verifica se há conteudo salvo na linha
+            $line = $line." ".$word;                           //adiciona a palavra na linha pulando um espaço
+          }else{                                            //se não houver conteudo na linha
+            $line = $word;                                     //coloca a palavra na linha (que estava vazia)
           }
           
-          // echo " | posWord: ".$line."</p>";
 
-        }else{
-          $line = $line."\n";
-          // echo "<p> lineAdd: ".$line."\n </p>";
-          $newText = $newText.$line;
-          $line = "";
-          $line = $line.$word;
-          
+        }else{                                         //se não houver espaço para a palavra na linha
+          $line = $line."\n";                           //adiciona quebra de linha na linha
+          $newText = $newText.$line;                    //grava a linha no texto
+          $line = "";                                   //limpa a linha
+          $line = $line.$word;                          //salva a palavra na "nova" linha
         }
       }
  
     }
-    if(strlen($line)>0){
-      $newText = $newText.$line;
+    if(strlen($line)>0){                                //ao acabar o foreach de palavras e houver conteúdo na linha, gravar no texto
+      $newText = $newText.$line;    
     }
-    // echo "<br><br> newText <br><br>";
-    echo nl2br($newText);
-    $textArray = explode ("\n", $newText);
+
+    $textArray = explode ("\n", $newText);              //separa a string $newText por "\n" em um array de strings (palavras)
     
-    return $textArray;
+    return $textArray;                                  //retorna o array com as palavras
   }
 
 }
